e44acb9b276caf3f1b03fcf1b1ab8cef1ced2234
luckyjanmatre <ljmatre15@gmail.com>
2026-01-20 22:40:49 +0800

    change

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+students = []
+
+@app.route("/", methods = ["GET", "POST"])
+def index():
+    if request.method == "POST":
+        name = request.form("name")
+        grade = float(request.form["grade"])
+        status = "Passed" if grade >= 75 else "Fail"
+
+        students.append({
+            "name": name,
+            "grade": grade,
+            "status": status
+        })
+        return redirect("/")
+    return render_template("index.html", students=students)
+
+if __name__ == "__main__":
+    app.run(debug=True)
