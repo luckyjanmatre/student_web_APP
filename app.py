50da1a3ad3a8d5f32381e489f9f7079cd8ce2d7e
luckyjanmatre <ljmatre15@gmail.com>
2026-01-27 11:54:19 +0800

    change

--- app.py
+++ app.py
@@ -1,23 +1,32 @@
 from flask import Flask, render_template, request, redirect
- 
+
 app = Flask(__name__)
- 
+
 students = []
- 
-@app.route("/", methods = ["GET", "POST"])
+
+@app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-        name = request.form("name")
+        name = request.form["name"]
         grade = float(request.form["grade"])
-        status = "Passed" if grade >= 75 else "Fail"
- 
+
+        if grade >= 75:
+            status = "Passed"
+            css_class = "pass"
+        else:
+            status = "Failed"
+            css_class = "fail"
+
         students.append({
             "name": name,
             "grade": grade,
-            "status": status
+            "status": status,
+            "class": css_class
         })
+
         return redirect("/")
+
     return render_template("index.html", students=students)
- 
+
 if __name__ == "__main__":
     app.run(debug=True)
